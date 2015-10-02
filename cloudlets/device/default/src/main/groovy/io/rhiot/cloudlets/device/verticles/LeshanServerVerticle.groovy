09b18f38654927519e4789dbb7146d230c4b1c94
Taariq Levack <taariql@gmail.com>
2015-10-02 10:28:24 +0200

    Verticle typo

--- cloudlets/device/default/src/main/groovy/io/rhiot/cloudlets/device/verticles/LeshanServerVerticle.groovy
+++ cloudlets/device/default/src/main/groovy/io/rhiot/cloudlets/device/verticles/LeshanServerVerticle.groovy
@@ -58,7 +58,7 @@ import static java.util.concurrent.TimeUnit.MINUTES
 import static org.eclipse.leshan.ResponseCode.CONTENT
 import static org.infinispan.configuration.cache.CacheMode.INVALIDATION_ASYNC
 
-class LeshanServerVeritcle extends GroovyVerticle {
+class LeshanServerVerticle extends GroovyVerticle {
 
     // Constants
 
@@ -94,7 +94,7 @@ class LeshanServerVeritcle extends GroovyVerticle {
 
     final def disconnectionPeriod = longProperty('disconnectionPeriod', DEFAULT_DISCONNECTION_PERIOD)
 
-    LeshanServerVeritcle() {
+    LeshanServerVerticle() {
         def cacheManager = new DefaultCacheManager(new GlobalConfigurationBuilder().transport().defaultTransport().build())
         Configuration builder = new ConfigurationBuilder().clustering().cacheMode(INVALIDATION_ASYNC).build();
         cacheManager.defineConfiguration("clients", builder);
